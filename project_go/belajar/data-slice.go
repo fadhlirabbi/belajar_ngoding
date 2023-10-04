fd06a9488cc5db7e3bdf96d0b8b2f48e72856cac
fadhli rabbi <fadhlirabbi46@.com>
2023-10-04 16:37:55 +0700

    lanjut

--- project_go/belajar/data-slice.go
+++ project_go/belajar/data-slice.go
@@ -30,4 +30,27 @@ func main() {
 	fmt.Println(slice1)
 	fmt.Println(len(slice1))
 	fmt.Println(cap(slice1))
+
+	fmt.Println("================")
+
+	var slice2 = month[10:]
+	fmt.Println(slice2)
+
+	var slice3 = append(slice2, "fadhli")
+	fmt.Println(slice3)
+	slice3[1] = "bukan desember"
+
+	fmt.Println(slice3)
+	fmt.Println(slice2)
+	fmt.Println(month)
+	// fmt.println
+
+	// days := [...]string{"senin", "selasa", "rabu", "kamis", "jumat", "sabtu"}
+	// daySlice1 := days[5:]
+
+	// // kita deklarasikan hari yang berbeda
+	// daySlice1[0] = "sabtu baru"
+	// daySlice1[1] = "minggu baru"
+
+	// fmt.Println(days)
 }
